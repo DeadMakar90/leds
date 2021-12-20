4cc459dbdbe078d4fd28631603e0ce08079a48d1
DeadMaka90 <deadmakar90yandex.ru>
2021-12-20 19:48:18 +0300

    v1.2 update

--- led_v1.2/led_v1.2.ino
+++ led_v1.2/led_v1.2.ino
@@ -1,4 +1,3 @@
-#include <AnalogKey.h>
 #include <GyverButton.h>
 #include <FastLED.h>
 
@@ -8,7 +7,7 @@
 #define COLOR_ORDER GRB //Цветовая схема ленты.
 #define CHIPSET WS2813 //Модель диодов
 #define BUTTON 5
-//int mode = 0;
+int mode = 0;
 CRGB leds[NUM_LEDS];  //Массив с размером = количеству диодов
 GButton butt1(BUTTON);
 
@@ -17,39 +16,52 @@ butt1.setDebounce(90);
 FastLED.addLeds<CHIPSET,DATA_PIN,COLOR_ORDER>(leds,NUM_LEDS); 
 FastLED.clear();
 FastLED.show();
-
+}
+void pin_ISR(){
+    mode++;
+    if (mode >=3){
+        mode = 0;
+    }
 }
 
 void loop(){ 
-butt1.tick(); 
-if (butt1.hasClicks()){
-byte mode = butt1.getClicks();
-if (mode >=3){mode=0;}
-switch(mode){
-case 1:
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-        leds[i] = CRGB :: Red;
-        FastLED.show();
-        delay(100);
+    switch(mode){
+        case 1: 
+        for ( int i = 0; i < NUM_LEDS; i++)
+        {
+            leds[i]=CRGB::Aqua;
+            FastLED.show();
+            delay(100);
+        }
+        for (int i = NUM_LEDS; i >=0 ; i--)
+        {
+            leds[i]=CRGB::Yellow;
+            FastLED.show();
+            delay(100);
+        }
+        break;
+        case 2: 
+             for (int i = 0; i < NUM_LEDS; i++)
+        {
+            leds[i]=CRGB::Red;
+            FastLED.show();
+            delay(100);
+        }
+        for (int i = NUM_LEDS; i >=0 ; i--)
+        {
+            leds[i]=CRGB::Green;
+            FastLED.show();
+            delay(100);
+        }
+        break;
+        default:
+         leds[0]=CRGB::Blue;
+         FastLED.show();
+         delay(100);
+         leds[0]=CRGB::Black;
+         FastLED.show();
+         delay(100);
+        break;
     }
-      for (int i = NUM_LEDS; i > 0; i++)
-    {
-        leds[i] = CRGB :: Green;
-        FastLED.show();
-        delay(100);
-    }
-    break;
-case 2: 
-     leds[0] = CRGB::GreenYellow;
-     FastLED.show();
-     delay(1000);
-     leds[1] = CRGB::Indigo;
-     FastLED.show();
-     delay(100);
-     break;
-default:
-    break;
-  }
- }
+
 }
